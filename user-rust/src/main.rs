fb53e9835483b38078e69aa25f32c46ad9f5e309
Philipp Schuster <phip1611@gmail.com>
2020-12-06 00:18:47 +0100

    rust: neli 0.5.1 (closes #2)

--- user-rust/src/main.rs
+++ user-rust/src/main.rs
@@ -1,100 +1,145 @@
 //! Make sure to make familiar with netlink and generic netlink first!
-//! I personally also just know a few things! If something is wrong please let me know!
-
+//!
 //! This code is the user part written in Rust that talks via a generic netlink family called
 //! "CONTROL_EXMPL" to a linux kernel module. It uses the "neli" lib/crate.
-//! 
+//!
+//! Imagine that you are developing an application or a software with multiple components
+//! (at least userland program and kernel module) that has several operations you want
+//! to trigger on the receiving side. These are "operations" or "commands" and referenced
+//! by an u8 (basically an enum) on the receiving side. Further you can specify
+//! attributes, the actual data you want to transfer.
+//!
+//! Our application here is called "Foobar", has "FoobarOperations" and "FoobarAttributes".
+//!
 //! Each netlink packet has a generic netlink packet as it's payload. In the generic netlink
 //! package one can define several attributes (type to value mappings).
-//! 
+//!
 //! Workflow is the following:
-//! 0) kernel module registers generic netlink family by name which creates a new 
+//! 0) kernel module registers generic netlink family by name which creates a new
 //!    virtual/temporary netlink family (a number)
-//! 1) client request family id of "CONTROL_EXMPL"
-//! 2) client sends data to retrieved family id.
+//! 1) client request family id of "CONTROL_EXMPL" (ONCE during application startup)
+//! 2) client sends data to retrieved family id. (Kernel module must be able to process it)
+//! 3) client can get replies
 
-#[macro_use]
-extern crate neli;
-
-use neli::socket;
-use neli::consts::{NlFamily, Cmd, NlAttrType, NlmF};
-use neli::genl::Genlmsghdr;
-use neli::nlattr::Nlattr;
-use neli::nl::Nlmsghdr;
+use neli::consts::nl::{NlmF, NlmFFlags};
+use neli::consts::socket::NlFamily;
+use neli::genl::{Genlmsghdr, Nlattr};
+use neli::nl::{NlPayload, Nlmsghdr};
+use neli::socket::NlSocketHandle;
+use neli::types::{Buffer, GenlBuffer};
+use neli::utils::U32Bitmask;
+use neli::Nl;
 use std::process;
 
-// Implements the necessary trait for the "neli" lib on an enum called "Command".
-// Command corresponds to "enum Commands" in kernel module C code.
+// Implements the necessary trait for the "neli" lib on an enum called "FoobarOperation".
+// FoobarOperation corresponds to "enum Commands" in kernel module C code.
 // Describes what callback function shall be invoked in the linux kernel module.
 // This is for the "cmd" field in Generic Netlink header.
-impl_var_trait!(
-    Command, u8, Cmd,
+neli::impl_var!(
+    FoobarOperation,
+    u8,
     Unspec => 0,
     Echo => 1,
     // we expect an error reply for this command
-    Echo_Fail => 2
+    EchoFail => 2
 );
+impl neli::consts::genl::Cmd for FoobarOperation {}
 
-// Implements the necessary trait for the "neli" lib on an enum called "ControlAttr".
-// Command corresponds to "enum Attributes" in kernel module C code.
+// Implements the necessary trait for the "neli" lib on an enum called "FoobarAttribute".
+// FoobarAttribute corresponds to "enum Attributes" in kernel module C code.
 // Describes the value type to data mappings inside the generic netlink packet payload.
 // This is for the Netlink Attributes (the actual payload) we want to send.
-impl_var_trait!(
-    ControlAttr, u16, NlAttrType,
+neli::impl_var!(
+    FoobarAttribute,
+    u16,
     Unspec => 0,
     Msg => 1
 );
+impl neli::consts::genl::NlAttrType for FoobarAttribute {}
 
 fn main() {
-    let mut socket = socket::NlSocket::connect(NlFamily::Generic, None, None, true).unwrap();
-    let family_id = socket.resolve_genl_family("CONTROL_EXMPL").expect("Generic Family must exist!");
+    let mut sock = NlSocketHandle::connect(
+        NlFamily::Generic,
+        // 0 is pid of kernel -> socket is connected to kernel
+        Some(0),
+        U32Bitmask::empty(),
+    )
+    .unwrap();
+
+    let family_id = sock
+        .resolve_genl_family("CONTROL_EXMPL")
+        .expect("Generic Family must exist!");
     println!("Generic family number is {}", family_id);
 
-    // Send String to linux kernel module
-    let str = String::from("Hello from userland (Rust)");
-    let attr = Nlattr::new(
-        Some(str.len() as u16),
-        // this type is the attribute type
-        ControlAttr::Msg,
-        str.clone()
-    ).unwrap();
-    let attrs = vec![attr];
-    let gen_nl_mhr = Genlmsghdr::new(Command::Echo/*_Fail*/, 1, attrs).unwrap();
-    let nl_msh_flags = vec![NlmF::Request];
-    let nl_msh = Nlmsghdr::new(
-        None,
-        // this type is the msg type (for example Ok or Error)
-        family_id,
-        //msg_type::OK,
-        nl_msh_flags,
-        None,
-        Some(process::id()),
-        gen_nl_mhr
+    let echo_msg = "Some data that has `Nl` trait implemented, like &str";
+
+    // We want to send an Echo command
+    // 1) prepare Foobar Attribute
+    let mut attrs: GenlBuffer<FoobarAttribute, Buffer> = GenlBuffer::new();
+    attrs.push(
+        Nlattr::new(
+            // when is this not None? TODO
+            None,
+            false,
+            // not sure, what this is TODO
+            false,
+            // the type of the attribute. This is an u16 and corresponds
+            // to an enum on the receiving side
+            FoobarAttribute::Msg,
+            echo_msg,
+        )
+        .unwrap(),
     );
-    println!("Sending '{}' via netlink", str);
-    socket.send_nl(nl_msh).unwrap();
+    // 2) prepare Generic Netlink Header. The Generic Netlink Header contains the
+    //    attributes (actual data) as payload.
+    let gnmsghdr = Genlmsghdr::new(
+        // FoobarOperation::Echo,
+        FoobarOperation::EchoFail,
+        // Version is dependent on your kernel module
+        // you can check it there and make special action, or you ignore it
+        1,
+        attrs,
+    );
+    // 3) Prepare Netlink header. The Netlink header contains the Generic Netlink header
+    //    as payload.
+    let nlmsghdr = Nlmsghdr::new(
+        None,
+        family_id,
+        // This depends on your kernel module. Do you check there if any flags are used?
+        // Request is required (TODO by neli or by netlink?)
+        // others are up to you
+        NlmFFlags::new(&[NlmF::Request]),
+        None,
+        // Port ID. Not necessarily the process id of the current process. This field
+        // could be used to identify different points or threads inside your application
+        // that send data to the kernel. This has nothing to do with "routing" the packet to
+        // the kernel
+        Some(process::id()),
+        NlPayload::Payload(gnmsghdr),
+    );
 
-    // ----------------------------------------
-    // Receive data;
-    // u16 is the family id type
-    let res = socket.recv_nl::<u16, Genlmsghdr::<Command, ControlAttr>>(None).unwrap();
+    println!("Sending '{}' via netlink", echo_msg);
+    // Send data
+    sock.send(nlmsghdr).expect("Send must work");
 
-    /// From <linux/netlink.h>
-    const NLMSG_ERROR: u16 = 0x2;
-    if res.nl_type == NLMSG_ERROR {
-        // nl_type is NLMSG_ERROR for "error messages" or
-        // the family number for "good messages"
-        panic!("We received an error!");
+    // receive echo'ed message
+    // TODO as of neli 0.5.1 .recv() is Err if nl_type of netlink header is 0x2 (NLMSG_ERR)
+    //  I think this is wrong. A well formed error message should be Ok.
+    let res: Nlmsghdr<u16, Genlmsghdr<FoobarOperation, FoobarAttribute>> =
+        sock.recv().expect("Should receive a message").unwrap();
+
+    // According to Netlink Linux kernel code nl_type is either 0x2 (error) or our family id (ok)
+    if res.nl_type == 0x2 {
+        // it actually depends on the kernel module if it replies with this nl_type
+        // depends on your specific use case
+        panic!("Kernel replied with error");
+    }
+    if res.nl_type == family_id {
+        println!("Received successful reply!");
     }
 
-    // iterate through all received attributes
-    res.nl_payload.get_attr_handle().iter().for_each(|attr| {
-        match attr.nla_type {
-           ControlAttr::Msg => {
-                println!("{}", String::from_utf8(attr.payload.clone()).unwrap());
-           },
-            _ => panic!()
-       }
-    });
+    let attr_handle = res.get_payload().unwrap().get_attr_handle();
+    let received = attr_handle.get_attribute(FoobarAttribute::Msg).unwrap();
+    let received = String::deserialize(received.nla_payload.as_ref()).unwrap();
+    println!("Received from kernel: '{}'", received);
 }
-

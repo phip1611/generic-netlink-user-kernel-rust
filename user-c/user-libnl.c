e64d763dd6aec4d40a931ea95b647110f2417f35
Philipp Schuster <phip1611@gmail.com>
2021-02-17 23:33:55 +0100

    prepared code in all three userland components to measure avg time in µs in code (issue #6)

--- user-c/user-libnl.c
+++ user-c/user-libnl.c
@@ -33,6 +33,7 @@
 #include <netlink/genl/ctrl.h> // for genl_ctrl_ functions
 // if we want to use functions flags like "NLM_F_ACK"
 #include <linux/netlink.h>
+#include <sys/time.h>
 
 // data/vars/enums/properties that describes our protocol that we implement
 // on top of generic netlink (like functions we want to trigger on the receiving side)
@@ -77,7 +78,7 @@ int nl_callback(struct nl_msg* recv_msg, void* arg)
     if (tb_msg[GNL_FOOBAR_XMPL_A_MSG]) {
         // parse it as string
         char * payload_msg = nla_get_string(tb_msg[GNL_FOOBAR_XMPL_A_MSG]);
-        printf(LOG_PREFIX "Kernel replied: %s\n", payload_msg);
+        // printf(LOG_PREFIX "Kernel replied: %s\n", payload_msg);
     }
 
     return NL_OK;
@@ -86,93 +87,101 @@ int nl_callback(struct nl_msg* recv_msg, void* arg)
 int main(void) {
     // ############################################################################################
     // ########## Step 1: Connect via generic netlink
+    struct timeval st, et;
+    gettimeofday(&st,NULL);
+    for (int i = 0; i < 100000; i++) {
 
-    // Allocate new netlink socket in memory.
-    struct nl_sock * socket = nl_socket_alloc();
+        // Allocate new netlink socket in memory.
+        struct nl_sock *socket = nl_socket_alloc();
 
-    // connect so that genl_ctrl_resolve can retrieve data
-    // equivalent to nl_connect(socket, NETLINK_GENERIC);
-    genl_connect(socket);
+        // connect so that genl_ctrl_resolve can retrieve data
+        // equivalent to nl_connect(socket, NETLINK_GENERIC);
+        genl_connect(socket);
 
-    // retrieve family id (kernel module registered a netlink family via generic netlink)
-    family_id = genl_ctrl_resolve(socket, FAMILY_NAME);
+        // retrieve family id (kernel module registered a netlink family via generic netlink)
+        family_id = genl_ctrl_resolve(socket, FAMILY_NAME);
 
-    if (family_id < 0) {
-        fprintf(stderr, LOG_PREFIX "generic netlink family '" FAMILY_NAME "' NOT REGISTERED\n");
+        if (family_id < 0) {
+            fprintf(stderr, LOG_PREFIX "generic netlink family '" FAMILY_NAME "' NOT REGISTERED\n");
+            nl_socket_free(socket);
+            exit(-1);
+        } else {
+            // printf(LOG_PREFIX "Family-ID of generic netlink family '" FAMILY_NAME "' is: %d\n", family_id);
+        }
+
+        // ############################################################################################
+        // ########## Step 2: Sending Data
+
+        // there is no reconnect with the new family necessary because this is already done by
+        // genl_ctrl_resolve(); see genl/ctrl.c
+        // nl_connect(socket, family_id);
+
+        // attach a callback
+        nl_socket_modify_cb(socket,
+                // btw: NL_CB_VALID doesn't work here;
+                // perhaps our kernel module sends somehow incomplete msgs?!
+                            NL_CB_MSG_IN, //  Called for every message received
+                            NL_CB_CUSTOM, // custom handler specified by us
+                            nl_callback, // function
+                            NULL // no argument to be passed to callback function
+        );
+
+        // we build a netlink package
+        // it's payload is the generic netlink header with its data
+        // nl message with default size
+        struct nl_msg *msg = nlmsg_alloc();
+        genlmsg_put(
+                msg,  // memory buffer
+                // Port ID. Not necessarily the process id of the current process. This field
+                // could be used to identify different points or threads inside your application
+                // that send data to the kernel. This has nothing to do with "routing" the packet to
+                // the kernel, because this is done by the socket itself
+                NL_AUTO_PORT, // auto assign current pid
+                // It is up to you if you want to split a data transfer into multiple sequences. (application specific)
+                NL_AUTO_SEQ, // begin wit seq number 0
+                family_id, // family id
+                0, // no additional user header
+                // You can use flags in an application specific way (e.g. ACK flag). It is up to you
+                // if you check against flags in your Kernel module. It is required to add NLM_F_REQUEST,
+                // otherwise the Kernel doesn't route the packet to the right Netlink callback handler
+                // in your Kernel module. This might result in a deadlock on the socket if an expected
+                // reply you are waiting for in a blocking way is never received.
+                // Kernel reference: https://elixir.bootlin.com/linux/v5.10.16/source/net/netlink/af_netlink.c#L2487
+                //
+                // libnl specific: always adds NLM_F_REQUEST
+                // see <libnl>/nl.c#nl_complete_msg()
+                NLM_F_REQUEST,
+                // cmd in Generic Netlink Header
+                GNL_FOOBAR_XMPL_C_ECHO, // the command we want to trigger on the receiving side
+                // You can evolve your application over time using different versions or ignore it.
+                // Application specific; receiver can check this value and to specific logic
+                1
+        );
+
+        NLA_PUT_STRING(msg, GNL_FOOBAR_XMPL_A_MSG, MESSAGE_TO_KERNEL);
+        int res = nl_send_auto(socket, msg);
+        nlmsg_free(msg);
+        if (res < 0) {
+            fprintf(stderr, LOG_PREFIX "sending message failed\n");
+        } else {
+            // printf(LOG_PREFIX "Sent to kernel: '%s'\n", MESSAGE_TO_KERNEL);
+        }
+
+        // ############################################################################################
+        // ########## Step 3: receive data
+
+        // wait for received messages and handle them according to our callback handlers
+        nl_recvmsgs_default(socket);
+
         nl_socket_free(socket);
-        exit(-1);
-    } else {
-        printf(LOG_PREFIX "Family-ID of generic netlink family '" FAMILY_NAME "' is: %d\n", family_id);
+        continue;
+
+    nla_put_failure: // referenced by NLA_PUT_STRING
+        nlmsg_free(msg);
+        return -1;
     }
-
-    // ############################################################################################
-    // ########## Step 2: Sending Data
-
-    // there is no reconnect with the new family necessary because this is already done by
-    // genl_ctrl_resolve(); see genl/ctrl.c
-    // nl_connect(socket, family_id);
-
-    // attach a callback
-    nl_socket_modify_cb(socket,
-                        // btw: NL_CB_VALID doesn't work here;
-                        // perhaps our kernel module sends somehow incomplete msgs?!
-                        NL_CB_MSG_IN, //  Called for every message received
-                        NL_CB_CUSTOM, // custom handler specified by us
-                        nl_callback, // function
-                        NULL // no argument to be passed to callback function
-    );
-
-    // we build a netlink package
-    // it's payload is the generic netlink header with its data
-    // nl message with default size
-    struct nl_msg * msg = nlmsg_alloc();
-    genlmsg_put(
-            msg,  // memory buffer
-            // Port ID. Not necessarily the process id of the current process. This field
-            // could be used to identify different points or threads inside your application
-            // that send data to the kernel. This has nothing to do with "routing" the packet to
-            // the kernel, because this is done by the socket itself
-            NL_AUTO_PORT, // auto assign current pid
-            // It is up to you if you want to split a data transfer into multiple sequences. (application specific)
-            NL_AUTO_SEQ, // begin wit seq number 0
-            family_id, // family id
-            0, // no additional user header
-            // You can use flags in an application specific way (e.g. ACK flag). It is up to you
-            // if you check against flags in your Kernel module. It is required to add NLM_F_REQUEST,
-            // otherwise the Kernel doesn't route the packet to the right Netlink callback handler
-            // in your Kernel module. This might result in a deadlock on the socket if an expected
-            // reply you are waiting for in a blocking way is never received.
-            // Kernel reference: https://elixir.bootlin.com/linux/v5.10.16/source/net/netlink/af_netlink.c#L2487
-            //
-            // libnl specific: always adds NLM_F_REQUEST
-            // see <libnl>/nl.c#nl_complete_msg()
-            NLM_F_REQUEST,
-            // cmd in Generic Netlink Header
-            GNL_FOOBAR_XMPL_C_ECHO, // the command we want to trigger on the receiving side
-            // You can evolve your application over time using different versions or ignore it.
-            // Application specific; receiver can check this value and to specific logic
-            1
-    );
-
-    NLA_PUT_STRING(msg, GNL_FOOBAR_XMPL_A_MSG, MESSAGE_TO_KERNEL);
-    int res = nl_send_auto(socket, msg);
-    nlmsg_free(msg);
-    if (res < 0) {
-        fprintf(stderr, LOG_PREFIX "sending message failed\n");
-    } else {
-        printf(LOG_PREFIX "Sent to kernel: '%s'\n", MESSAGE_TO_KERNEL);
-    }
-
-    // ############################################################################################
-    // ########## Step 3: receive data
-
-    // wait for received messages and handle them according to our callback handlers
-    nl_recvmsgs_default(socket);
-
-    nl_socket_free(socket);
+    gettimeofday(&et,NULL);
+    long avg_duration_us = (((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec)) / 100000;
+    printf(LOG_PREFIX "Took on average %ldµs\n", avg_duration_us);
     return 0;
-
-nla_put_failure: // referenced by NLA_PUT_STRING
-    nlmsg_free(msg);
-    return -1;
 }
--- user-rust/src/main.rs
+++ user-rust/src/main.rs
@@ -35,6 +35,7 @@ use neli::{
 };
 use std::process;
 use std::process::exit;
+use std::time::Instant;
 
 /// Name of the Netlink family registered via Generic Netlink
 const FAMILY_NAME: &str = "gnl_foobar_xmpl";
@@ -67,95 +68,100 @@ neli::impl_var!(
 impl neli::consts::genl::NlAttrType for NlFoobarXmplAttribute {}
 
 fn main() {
-    let mut sock = NlSocketHandle::connect(
-        NlFamily::Generic,
-        // 0 is pid of kernel -> socket is connected to kernel
-        Some(0),
-        U32Bitmask::empty(),
-    )
-    .unwrap();
-
-    let family_id;
-    let res = sock.resolve_genl_family(FAMILY_NAME);
-    match res {
-        Ok(id) => family_id = id,
-        Err(e) => {
-            eprintln!(
-                "The Netlink family '{}' can't be found. Is the kernel module loaded yet? neli-error='{}'",
-                FAMILY_NAME, e
-            );
-            // exit without error in order for Continuous Integration and automatic testing not to fail
-            // when the kernel module is not loaded
-            exit(0);
-        }
-    }
-
-    println!("[User-Rust]: Generic family number is {}", family_id);
-
-    // We want to send an Echo command
-    // 1) prepare NlFoobarXmpl Attribute
-    let mut attrs: GenlBuffer<NlFoobarXmplAttribute, Buffer> = GenlBuffer::new();
-    attrs.push(
-        Nlattr::new(
-            None,
-            false,
-            false,
-            // the type of the attribute. This is an u16 and corresponds
-            // to an enum on the receiving side
-            NlFoobarXmplAttribute::Msg,
-            ECHO_MSG,
+    let begin = Instant::now();
+    for _ in 0..100000 {
+        let mut sock = NlSocketHandle::connect(
+            NlFamily::Generic,
+            // 0 is pid of kernel -> socket is connected to kernel
+            Some(0),
+            U32Bitmask::empty(),
         )
-        .unwrap(),
-    );
-    // 2) prepare Generic Netlink Header. The Generic Netlink Header contains the
-    //    attributes (actual data) as payload.
-    let gnmsghdr = Genlmsghdr::new(
-        NlFoobarXmplOperation::Echo,
-        // You can evolve your application over time using different versions or ignore it.
-        // Application specific; receiver can check this value and to specific logic
-        1,
-        // actual payload
-        attrs,
-    );
-    // 3) Prepare Netlink header. The Netlink header contains the Generic Netlink header
-    //    as payload.
-    let nlmsghdr = Nlmsghdr::new(
-        None,
-        family_id,
-        // You can use flags in an application specific way (e.g. ACK flag). It is up to you
-        // if you check against flags in your Kernel module. It is required to add NLM_F_REQUEST,
-        // otherwise the Kernel doesn't route the packet to the right Netlink callback handler
-        // in your Kernel module. This might result in a deadlock on the socket if an expected
-        // reply you are waiting for in a blocking way is never received.
-        // Kernel reference: https://elixir.bootlin.com/linux/v5.10.16/source/net/netlink/af_netlink.c#L2487
-        NlmFFlags::new(&[NlmF::Request]),
-        // It is up to you if you want to split a data transfer into multiple sequences. (application specific)
-        None,
-        // Port ID. Not necessarily the process id of the current process. This field
-        // could be used to identify different points or threads inside your application
-        // that send data to the kernel. This has nothing to do with "routing" the packet to
-        // the kernel, because this is done by the socket itself
-        Some(process::id()),
-        NlPayload::Payload(gnmsghdr),
-    );
+            .unwrap();
 
-    println!("[User-Rust]: Sending '{}' via netlink", ECHO_MSG);
+        let family_id;
+        let res = sock.resolve_genl_family(FAMILY_NAME);
+        match res {
+            Ok(id) => family_id = id,
+            Err(e) => {
+                eprintln!(
+                    "The Netlink family '{}' can't be found. Is the kernel module loaded yet? neli-error='{}'",
+                    FAMILY_NAME, e
+                );
+                // exit without error in order for Continuous Integration and automatic testing not to fail
+                // when the kernel module is not loaded
+                exit(0);
+            }
+        }
 
-    // Send data
-    sock.send(nlmsghdr).expect("Send must work");
+        // println!("[User-Rust]: Generic family number is {}", family_id);
 
-    // receive echo'ed message
-    let res: Nlmsghdr<u16, Genlmsghdr<NlFoobarXmplOperation, NlFoobarXmplAttribute>> =
-        sock.recv().expect("Should receive a message").unwrap();
+        // We want to send an Echo command
+        // 1) prepare NlFoobarXmpl Attribute
+        let mut attrs: GenlBuffer<NlFoobarXmplAttribute, Buffer> = GenlBuffer::new();
+        attrs.push(
+            Nlattr::new(
+                None,
+                false,
+                false,
+                // the type of the attribute. This is an u16 and corresponds
+                // to an enum on the receiving side
+                NlFoobarXmplAttribute::Msg,
+                ECHO_MSG,
+            )
+                .unwrap(),
+        );
+        // 2) prepare Generic Netlink Header. The Generic Netlink Header contains the
+        //    attributes (actual data) as payload.
+        let gnmsghdr = Genlmsghdr::new(
+            NlFoobarXmplOperation::Echo,
+            // You can evolve your application over time using different versions or ignore it.
+            // Application specific; receiver can check this value and to specific logic
+            1,
+            // actual payload
+            attrs,
+        );
+        // 3) Prepare Netlink header. The Netlink header contains the Generic Netlink header
+        //    as payload.
+        let nlmsghdr = Nlmsghdr::new(
+            None,
+            family_id,
+            // You can use flags in an application specific way (e.g. ACK flag). It is up to you
+            // if you check against flags in your Kernel module. It is required to add NLM_F_REQUEST,
+            // otherwise the Kernel doesn't route the packet to the right Netlink callback handler
+            // in your Kernel module. This might result in a deadlock on the socket if an expected
+            // reply you are waiting for in a blocking way is never received.
+            // Kernel reference: https://elixir.bootlin.com/linux/v5.10.16/source/net/netlink/af_netlink.c#L2487
+            NlmFFlags::new(&[NlmF::Request]),
+            // It is up to you if you want to split a data transfer into multiple sequences. (application specific)
+            None,
+            // Port ID. Not necessarily the process id of the current process. This field
+            // could be used to identify different points or threads inside your application
+            // that send data to the kernel. This has nothing to do with "routing" the packet to
+            // the kernel, because this is done by the socket itself
+            Some(process::id()),
+            NlPayload::Payload(gnmsghdr),
+        );
 
-    /* USELESS, just note: this is always the case. Otherwise neli would have returned Error
-    if res.nl_type == family_id {
-        println!("Received successful reply!");
-    }*/
+        // println!("[User-Rust]: Sending '{}' via netlink", ECHO_MSG);
 
-    let attr_handle = res.get_payload().unwrap().get_attr_handle();
-    let received = attr_handle
-        .get_attr_payload_as::<String>(NlFoobarXmplAttribute::Msg)
-        .unwrap();
-    println!("[User-Rust]: Received from kernel: '{}'", received);
+        // Send data
+        sock.send(nlmsghdr).expect("Send must work");
+
+        // receive echo'ed message
+        let res: Nlmsghdr<u16, Genlmsghdr<NlFoobarXmplOperation, NlFoobarXmplAttribute>> =
+            sock.recv().expect("Should receive a message").unwrap();
+
+        /* USELESS, just note: this is always the case. Otherwise neli would have returned Error
+        if res.nl_type == family_id {
+            println!("Received successful reply!");
+        }*/
+
+        let attr_handle = res.get_payload().unwrap().get_attr_handle();
+        let received = attr_handle
+            .get_attr_payload_as::<String>(NlFoobarXmplAttribute::Msg)
+            .unwrap();
+        // println!("[User-Rust]: Received from kernel: '{}'", received);
+    }
+    let us = begin.elapsed().as_micros() / 100000;
+    println!("took {}µs on average", us);
 }

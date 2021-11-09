40506c4926e51b30e4873b8dcb9fac68a13e2ec0
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2021-11-09 17:59:53 +0100

    neli v0.6.0 prepared

--- user-rust/src/bin/reply_with_error.rs
+++ user-rust/src/bin/reply_with_error.rs
@@ -1,22 +1,22 @@
 /* Copyright 2021 Philipp Schuster
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in the
- * Software without restriction, including without limitation the rights to use, copy,
- * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so, subject to the
- * following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies
- * or substantial portions of the Software.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in the
+* Software without restriction, including without limitation the rights to use, copy,
+* modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+* and to permit persons to whom the Software is furnished to do so, subject to the
+* following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all copies
+* or substantial portions of the Software.
 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 //! Userland component written in Rust, that uses neli to talk to a custom Netlink
 //! family via Generic Netlink. The family is called "gnl_foobar_xmpl" and the
@@ -42,10 +42,6 @@ use user_rust::{NlFoobarXmplAttribute, NlFoobarXmplCommand, FAMILY_NAME};
 fn main() {
     println!("Rust-Binary: reply_with_error");
 
-    eprintln!("##  ATTENTION ###################################################################################");
-    eprintln!("TODO Rust error receiving fails until https://github.com/jbaublitz/neli/issues/116 gets resolved!");
-    eprintln!("#################################################################################################");
-
     let mut sock = NlSocketHandle::connect(
         NlFamily::Generic,
         // 0 is pid of kernel -> socket is connected to kernel
@@ -60,7 +56,7 @@ fn main() {
         Ok(id) => family_id = id,
         Err(e) => {
             eprintln!(
-                "The Netlink family '{}' can't be found. Is the kernel module loaded yet? neli-error='{}'",
+                "The Netlink family '{}' can't be found. Is the kernel module loaded yet? neli-error='{:#?}'",
                 FAMILY_NAME, e
             );
             // exit without error in order for Continuous Integration and automatic testing not to fail
@@ -73,7 +69,6 @@ fn main() {
     let mut attrs: GenlBuffer<NlFoobarXmplAttribute, Buffer> = GenlBuffer::new();
     attrs.push(
         Nlattr::new(
-            None,
             false,
             false,
             NlFoobarXmplAttribute::Msg,
@@ -95,7 +90,7 @@ fn main() {
 
     let res: Result<
         Option<Nlmsghdr<u16, Genlmsghdr<NlFoobarXmplCommand, NlFoobarXmplAttribute>>>,
-        NlError,
+        NlError<u16, Genlmsghdr<NlFoobarXmplCommand, NlFoobarXmplAttribute>>,
     > = sock.recv();
     let received_err = res.unwrap_err();
     let received_err = match received_err {
@@ -106,17 +101,14 @@ fn main() {
         "Inside the kernel this error code occurred: {}",
         received_err.error
     );
-    let nlmsg_that_caused_error = received_err
-        .nlmsg
-        .get_payload_as::<Genlmsghdr<NlFoobarXmplCommand, NlFoobarXmplAttribute>>();
-    let nlmsg_that_caused_error = nlmsg_that_caused_error.expect("Must contain original payload");
-    let attr_handle = nlmsg_that_caused_error.get_attr_handle();
+    let attr_handle = received_err.nlmsg.nl_payload.get_attr_handle();
     println!(
         "ECHO-attribute was: {}",
         attr_handle
-            .get_attr_payload_as::<String>(NlFoobarXmplAttribute::Msg)
+            .get_attr_payload_as_with_len::<String>(NlFoobarXmplAttribute::Msg)
             .expect("This attribute was sent and must be in the response")
     );
+    println!("NlMsg that caused the error: {:#?}", received_err.nlmsg);
 
     println!("Everything okay; successfully received error")
 }
